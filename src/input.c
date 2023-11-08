6be0d31f2b34ed7aee8007cc08d6926394d4518f
deathy <deathy.epl.ccs@gmail.com>
2023-11-08 10:37:42 -0800

    Documented input.h/c

--- src/input.c
+++ src/input.c
@@ -1,3 +1,8 @@
+/// \file	input.c
+/// 
+/// \brief	This file contains the code and variables for handling player input ///
+/// \copyright 2023 by BAS and deathy (AKA Clifford A. Anderson).  
+/// All rights reserved.
 #include "input.h"
 
 
@@ -5,83 +10,97 @@
 #include "gameloop.h"
 
 
+/// \brief	This procedure processes the player input
+///
+/// This procedure processes all of the player input, mergin the input of both
+/// joysticks as well as keyboard controls into a single input bitmask to allow
+/// us to be input agnostic (not counting mouse and paddles, of course).
 void process_input() {
-	// is M pressed?
+	// is M pressed?  This is a special keyboard command for muting the music
 	KEYSCAN.MATRIXPEEK = 0x04;
 	if ((KEYSCAN.CRTACSCNT & (0x01 << 0x04)) == 0) {
+		// if it is pressed, and it wasn't the last time we test it,
 		if (!key_down) {
-			if (muted) {
-				if (current_loaded_state == 0) {
-					SID1.VOLUME_FTYPE = 0x3f;
-					SID2.VOLUME_FTYPE = 0x74;
-					SID3.VOLUME_FTYPE = 0x7f;
-					SID4.VOLUME_FTYPE = 0x2f;
-				} else {
-					SID1.VOLUME_FTYPE = 0x6f;
-					SID2.VOLUME_FTYPE = 0x6a;
-					SID3.VOLUME_FTYPE = 0x2b;
-					SID4.VOLUME_FTYPE = 0x3b;
-				}
-			} else {
-				if (current_loaded_state == 0) {
-					SID1.VOLUME_FTYPE = 0x30;
-					SID2.VOLUME_FTYPE = 0x70;
-					SID3.VOLUME_FTYPE = 0x70;
-					SID4.VOLUME_FTYPE = 0x20;
-				} else {
-					SID1.VOLUME_FTYPE = 0x60;
-					SID2.VOLUME_FTYPE = 0x60;
-					SID3.VOLUME_FTYPE = 0x20;
-					SID4.VOLUME_FTYPE = 0x30;
-				}
-			}
+			// then we can toggle the muting
 			muted = ~muted;
+			// and flag that the M key is down, so that we can wait for the key
+			// to be released before toggline again.  This is so we don't very
+			// rapidly toggle the music on and off every frame this input is
+			// called
 			key_down = 1;
 		}
 	} else {
+		// otherwise, if the M key is not pressed, then we clear the key_down
+		// flag.
 		key_down = 0;
 	}
 	
-	// load joystick two
+	// load joystick two.  we mask it to just the relevant bits, and we also
+	// invert the values because the default is for the bits being off to
+	// indicate movement and fire button.  we invert it to make the code more
+	// readable elsewhere without having to constantly explain this oddity
+	// inherited from the c64.
 	player_input = ~CIA1.PORTA & 0x1f;
 	
-	// load joystick one
+	// load joystick one.  masked and inverted just like the joystick in port 2
 	player_input |= ~CIA1.PORTB & 0x1f;
 	
-	// is W pressed?
+	// the order of how the keys are processed is to avoid changing the fields
+	// more than necessary.
+	
+	// is W pressed?  set the up bit
 	KEYSCAN.MATRIXPEEK = 0x01;
 	if ((KEYSCAN.CRTACSCNT & (0x01 << 0x01)) == 0) {
 		player_input |= 0b00000001;
 	}
 	
-	// is S pressed?
+	// is S pressed?  set the down bit
 	if ((KEYSCAN.CRTACSCNT & (0x01 << 0x05)) == 0) {
 		player_input |= 0b00000010;
 	}
 	
-	// is A pressed?
+	// is A pressed?  set the left bit
 	if ((KEYSCAN.CRTACSCNT & (0x01 << 0x02)) == 0) {
 		player_input |= 0b00000100;
 	}
 	
-	// is left shift pressed?
+	// is left shift pressed? set the fire bit
 	if ((KEYSCAN.CRTACSCNT & (0x01 << 0x07)) == 0) {
 		player_input |= 0b00010000;
 	}
 
-	// is D pressed?
+	// is D pressed?  set the right bit
 	KEYSCAN.MATRIXPEEK = 0x02;
 	if ((KEYSCAN.CRTACSCNT & (0x01 << 0x02)) == 0) {
 		player_input |= 0b00001000;
 	}
 	
-	// is right shift pressed?
+	// is right shift pressed?  set the fire bit
 	KEYSCAN.MATRIXPEEK = 0x06;
 	if ((KEYSCAN.CRTACSCNT & (0x01 << 0x04)) == 0) {
 		player_input |= 0b00010000;
 	}
 }
 
+/// \brief	This variable contains the player input
+///
+/// This variable contains the player input, using a bit mask as follows:
+///
+/// Mask      | Input Result
+/// --------- | ------------
+/// %00000001 | Up
+/// %00000010 | Down
+/// %00000100 | Left
+/// %00001000 | Right
+/// %00010000 | Fire
+///
+/// This is basically the same bitmask as the CIA joystick bitmask, only 
+/// inverted and without using the top 3 bits.
 char player_input = 0;
+
+/// \brief	This is used to wait for the M key to be released before responding
+///			to it again.
 char key_down     = 0;
+
+/// \brief	Whether or not the music is muted.
 char muted;
--- src/input.h
+++ src/input.h
@@ -1,3 +1,10 @@
+/// \file	input.h
+/// 
+/// \brief	This file is the header file that declares the input handler and
+///			related variables
+///
+/// \copyright 2023 by BAS and deathy (AKA Clifford A. Anderson).  
+/// All rights reserved.
 #ifndef __INPUT_H
 #define __INPUT_H
 
