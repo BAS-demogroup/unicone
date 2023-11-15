907fae0eb5a53fa90c23ad3a7e0142bffaa4ded9
deathy <deathy.epl.ccs@gmail.com>
2023-11-15 10:24:21 -0800

    Tweak to level 8 difficulty

--- src/difficulty.c
+++ src/difficulty.c
@@ -263,7 +263,7 @@ char falling_speed;
 
 /// \brief	This is a level table for unicorn frame rates
 char unicorn_frame_rates[24] = {
-	4,4,3,4,3,3,4,3,
-	4,4,3,3,2,2,3,2,
-	3,3,2,2,1,1,2,1
+	4,4,3,4,3,3,4,4,
+	4,4,3,3,2,2,3,3,
+	3,3,2,2,1,1,2,2
 };
